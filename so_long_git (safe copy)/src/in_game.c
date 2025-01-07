cc2a9975f75397842d115e49a278df9ddfb97793
Joao Victor Brol Carneiro <jbrol-ca@c1r7s2.42porto.com>
2025-01-07 22:24:30 +0000

    only norminette main left

--- so_long_git (safe copy)/src/in_game.c
+++ so_long_git (safe copy)/src/in_game.c
@@ -1,46 +1,55 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   in_game.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jbrol-ca <jbrol-ca@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/07 22:21:01 by jbrol-ca          #+#    #+#             */
+/*   Updated: 2025/01/07 22:23:56 by jbrol-ca         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "so_long.h"
 
 int	check_exit_move(t_game *game, int keycode, int *new_x, int *new_y)
 {
-    if (game->map[*new_y][*new_x] == 'E')
-    {
-        if (are_collectibles_collected(game->map))
-        {
-            cleanup_game(game);
-            exit(0);  // Exit the game
-        }
-        update_adjacent_cell(keycode, new_x, new_y, game);
-    }
-    return (1);
+	if (game->map[*new_y][*new_x] == 'E')
+	{
+		if (are_collectibles_collected(game->map))
+		{
+			cleanup_game(game);
+			exit(0);
+		}
+		update_adjacent_cell(keycode, new_x, new_y, game);
+	}
+	return (1);
 }
 
 void	update_adjacent_cell(int keycode, int *new_x, int *new_y, t_game *game)
 {
-    int next_x = *new_x;
-    int next_y = *new_y;
+	int	next_x;
+	int	next_y;
 
-    if (keycode == KEY_UP)
-        next_y -= 1;
-    else if (keycode == KEY_DOWN)
-        next_y += 1;
-    else if (keycode == KEY_LEFT)
-        next_x -= 1;
-    else if (keycode == KEY_RIGHT)
-        next_x += 1;
-
-    if (is_valid_move(game, next_x, next_y))
-    {
-        *new_x = next_x;
-        *new_y = next_y;
-    }
+	next_x = *new_x;
+	next_y = *new_y;
+	if (keycode == KEY_UP)
+		next_y -= 1;
+	else if (keycode == KEY_DOWN)
+		next_y += 1;
+	else if (keycode == KEY_LEFT)
+		next_x -= 1;
+	else if (keycode == KEY_RIGHT)
+		next_x += 1;
+	if (is_valid_move(game, next_x, next_y))
+	{
+		*new_x = next_x;
+		*new_y = next_y;
+	}
 }
 
-
-
-
-// Function to increment valid movements counter
-void count_valid_move(t_game *game)
+void	count_valid_move(t_game *game)
 {
-    game->valid_movements++;
-    ft_printf("Valid movement. Total valid moves: %d\n", game->valid_movements);
+	game->valid_movements++;
+	ft_printf("Valid movement. Total valid moves: %d\n", game->valid_movements);
 }
